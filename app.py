8f8de18b6d508d9a6806841ebc73e07f4e7b11ec
jean reis <jvictor4919@gmail.com>
2020-04-30 17:14:14 -0300

    update

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    usuarios = [
+        ['joao', 'joao@gmail.com'],
+        ['jose', 'jose@gmail.com'],
+        ['Maria', 'maria@gmail.com']
+    ] 
+    return render_template(
+        'index.html',
+        titulo = 'Index',
+        usuarios = usuarios
+        )
 
 if __name__ == '__main__':     
     app.run()
